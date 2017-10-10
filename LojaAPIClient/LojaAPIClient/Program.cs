be284324f8c517dab9773504f9f8b0f8e448483f
Andre Chaves <andrechavesg@gmail.com>
2017-10-10 17:27:39 -0300

    capitulo 8

--- LojaAPIClient/LojaAPIClient/Program.cs
+++ LojaAPIClient/LojaAPIClient/Program.cs
@@ -12,25 +12,12 @@ namespace LojaAPIClient
     {
         static void Main(string[] args)
         {
-            string conteudo;
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:portaDoSeuServidor/api/carrinho");
-            request.Method = "POST";
-            request.ContentType = "application/xml";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:portaDoSeuServidor/api/carrinho/1/produto/3467");
+            request.Method = "DELETE";
 
-            string xml = "<Carrinho xmlns:i='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://schemas.datacontract.org/2004/07/Loja.Models'><Endereco>Rua Vergueiro 3185, 8 andar, Sao Paulo</Endereco><Id>1</Id><Produtos><Produto><Id>123</Id><Nome>Produto criado com POST</Nome><Preco>100</Preco><Quantidade>1</Quantidade></Produto></Produtos></Carrinho>";
-            byte[] xmlBytes = Encoding.UTF8.GetBytes(xml);
-            request.GetRequestStream().Write(xmlBytes, 0, xmlBytes.Length);
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
 
-            request.ContentType = "application/xml";
-
-            WebResponse response = request.GetResponse();
-            using (Stream responseStream = response.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
-                conteudo = reader.ReadToEnd();
-            }
-
-            Console.Write(conteudo);
+            Console.Write(response.StatusCode);
             Console.Read();
         }
 
